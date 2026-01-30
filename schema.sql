ec039fbac59757460e7337bdcec8b65ddf36952d
dsa83171 <dsa83171@gmail.com>
2026-01-30 10:22:00 +0800

    add col

--- schema.sql
+++ schema.sql
@@ -1,10 +1,19 @@
 CREATE TABLE IF NOT EXISTS clips (
   id TEXT PRIMARY KEY,
   url TEXT,
+  embed_url TEXT,
+  broadcaster_id TEXT,
   broadcaster_name TEXT,
+  creator_id TEXT,
   creator_name TEXT,
+  video_id TEXT,
+  game_id TEXT,
+  language TEXT,
   title TEXT,
   view_count INTEGER,
   created_at TEXT,
-  thumbnail_url TEXT
+  thumbnail_url TEXT,
+  duration REAL,
+  vod_offset REAL,
+  is_featured BOOLEAN
 );
